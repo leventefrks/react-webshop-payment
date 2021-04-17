41e0bc5770c49643d2643ed77d0621b2ca29198d
leventefrks <x-farkasle@wizzair.com>
2021-04-17 18:19:49 +0200

    CardLogo

---
+++ src/components/CardLogo.js
@@ -0,0 +1,15 @@
+import { CREDIT_CARD_TYPES, CREDIT_CARD_TYPE_VISA } from './../constants';
+
+const CardLogo = ({ type }) => {
+  const getCardType = CREDIT_CARD_TYPES.get(type || CREDIT_CARD_TYPE_VISA);
+
+  return (
+    <img
+      src={require(`./../assets/${getCardType}.svg`).default}
+      alt={getCardType}
+      className="absolute top-4 right-4 w-10 h-10"
+    />
+  );
+};
+
+export default CardLogo;
--- src/components/CreditCard.js
+++ src/components/CreditCard.js
@@ -1,11 +1,7 @@
 import PropTypes from 'prop-types';
 import { SiContactlesspayment } from 'react-icons/si';
-import {
-  CARD_PLACEHOLDER_CVV,
-  CARD_PLACEHOLDER_NAME,
-  CREDIT_CARD_TYPES,
-  CREDIT_CARD_TYPE_VISA,
-} from './../constants';
+import { CARD_PLACEHOLDER_CVV, CARD_PLACEHOLDER_NAME } from './../constants';
+import CardLogo from './CardLogo';
 
 const CreditCard = ({
   cardType,
@@ -15,8 +11,6 @@ const CreditCard = ({
   expirationDate,
   cvv,
 }) => {
-  const getCardType = CREDIT_CARD_TYPES.get(cardType);
-
   return (
     <div className="card-container w-96 h-56 -mt-28 relative text-white duration-300 transform hover:scale-110">
       <div
@@ -25,22 +19,18 @@ const CreditCard = ({
       >
         <div className="absolute front w-full h-56 rounded-xl bg-gradient-to-r from-indigo-500 to-pink-700 shadow-2xl">
           <div className="w-full h-full flex flex-col justify-between p-6">
-            <img
-              src={require(`./../assets/${getCardType}.svg`).default}
-              alt={getCardType}
-              className="absolute top-4 right-4 w-8 h-8"
-            />
+            <CardLogo type={cardType} />
             <div className="flex justify-between">
               <div>
                 <span className="credit-card-label">Name</span>
-                <p className="credit-card-input">
+                <div className="credit-card-input">
                   {cardHolderName ? cardHolderName : CARD_PLACEHOLDER_NAME}
-                </p>
+                </div>
               </div>
             </div>
             <div>
-              <p className="credit-card-label">Card Number</p>
-              <p className="credit-card-input">{cardPlaceholder}</p>
+              <div className="credit-card-label">Card Number</div>
+              <div className="credit-card-input">{cardPlaceholder}</div>
             </div>
           </div>
         </div>
@@ -48,14 +38,14 @@ const CreditCard = ({
           <div className="absolute top-10 w-full bg-gray-700 h-8"></div>
           <div className="w-full flex justify-between mx-6 mb-6 ">
             <div className="flex-1">
-              <p className="credit-card-label">Expiry</p>
-              <p className="credit-card-input">{expirationDate}</p>
+              <div className="credit-card-label">Expiry</div>
+              <div className="credit-card-input">{expirationDate}</div>
             </div>
             <div className="flex-1">
-              <p className="credit-card-label uppercase">cvv</p>
-              <p className="credit-card-input">
+              <div className="credit-card-label uppercase">cvv</div>
+              <div className="credit-card-input">
                 {cvv ? cvv : CARD_PLACEHOLDER_CVV}
-              </p>
+              </div>
             </div>
           </div>
           <SiContactlesspayment className="absolute top-4 right-4 w-10 h-10" />
